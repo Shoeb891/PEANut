5dd2d40c05672be8c0538a92fc2940384ac0db75
iamsadat <sadatullahkhan40@gmail.com>
2023-08-18 04:24:05 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
-console.log("Backend");
+const express = require("express");
+const app = express();
+const port = 3001;
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
