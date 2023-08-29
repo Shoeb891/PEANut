3820d72ba81005faad2871fbd057b7f701c9487f
shoebm891 <shoebm891@gmail.com>
2023-08-30 02:55:43 +0530

    signup route

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,48 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+import { NextRequest,NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+export async function POST(request:NextRequest) {
+    try {
+        
+        const reqbody = await request.json();
+        const {rollnumber,email,password} = reqbody;
+
+        console.log(reqbody);
+
+        const existingUser = await prisma.user.findFirst({
+            where:{
+                email:email,
+            },
+        });
+
+        if(existingUser){
+            return NextResponse.json(
+            {message:"user already exists"},
+            {status:400}
+            );
+        }
+        
+        const salt = await bcryptjs.genSalt(10)
+        const hashedPassword = await bcryptjs.hash(password, salt)
+
+        const newUser = await prisma.user.create({
+            data:{
+                rollnumber,
+                email,
+                password:hashedPassword
+            },
+        });
+
+        return NextResponse.json({ message: "User created Successfully!", success:true });
+
+        
+    } catch (error:any) {
+        return NextResponse.json({error:error.message},
+            {status:500})
+    }
+    
+}
+
