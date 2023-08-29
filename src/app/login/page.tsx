0a6dc69202c18f60d9e96cce8bfaeb49cb03fe4e
shoebm891 <shoebm891@gmail.com>
2023-08-30 00:15:12 +0530

    Added Signup Login pages

---
+++ src/app/login/page.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+
+
+function LogIn() {
+    const [data, setData] = useState({
+      rollNumber: "",
+      password: "",
+    });
+  
+    const router = useRouter();
+  
+    // const { data: session } = useSession();
+  
+    const handleSubmit = async (e: any) => {
+    //   e.preventDefault();
+    //   signIn("credentials", {
+    //     ...data,
+    //     redirect: false,
+    //   });
+    //   router.push("/dashboard");
+    };
+  
+    return (
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-white">
+            Log In to your account
+          </h2>
+        </div>
+  
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div>
+              <label
+                htmlFor="rollNumber"
+                className="block text-sm font-medium leading-6 text-gray-900 dark:text-white"
+              >
+                Roll Number
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="text"
+                  autoComplete="email"
+                  placeholder="Enter roll number"
+                  required
+                  value={data.rollNumber}
+                  onChange={(e) =>
+                    setData({ ...data, rollNumber: e.target.value })
+                  }
+                  className="block w-full rounded-md border-0 py-1.5 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+  
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900 dark:text-white"
+                >
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-semibold text-gray-700 dark:text-gray-300 hover:text-black dark:hover:text-white"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  placeholder="Enter password"
+                  required
+                  value={data.password}
+                  onChange={(e) => setData({ ...data, password: e.target.value })}
+                  className="block w-full rounded-md border-0 py-1.5 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+  
+            <div>
+              <Button
+                type="submit"
+                className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white dark:text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+              >
+                Log In
+              </Button>
+              <div className=" py-3 text-sm">
+                <Link
+                  href="/signup"
+                  className="font-semibold text-gray-700 dark:text-gray-300 hover:text-black dark:hover:text-white"
+                >
+                  {"Don't have an account? Register"}
+                </Link>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+                }
+  export default LogIn;
