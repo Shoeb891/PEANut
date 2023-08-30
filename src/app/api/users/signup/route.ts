955d69afc8322170f52617c25da75c641040e34a
iamsadat <sadatullahkhan40@gmail.com>
2023-08-30 14:48:24 +0530

    Hidden env file

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,49 +1,44 @@
-import { Prisma, PrismaClient } from "@prisma/client";
-import { NextRequest,NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
 
 const prisma = new PrismaClient();
 
-export async function POST(request:NextRequest) {
-    try {
-        
-        const reqbody = await request.json();
-        const {name,rollnumber,email,password} = reqbody;
-        const salt = await bcryptjs.genSalt(10)
-        const hashedPassword = await bcryptjs.hash(password, salt)
-        console.log(reqbody);
+export async function POST(request: NextRequest) {
+  try {
+    const reqbody = await request.json();
+    const { name, rollnumber, email, password } = reqbody;
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+    console.log(reqbody);
 
-        const existingUser = await prisma.user.findFirst({
-            where:{
-                email:email,
-            },
-        });
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
 
-        if(existingUser){
-            return NextResponse.json(
-            {message:"user already exists"},
-            {status:400}
-            );
-        }
-        
-        
-
-        const newUser = await prisma.user.create({
-            data:{
-                name,
-                rollnumber,
-                email,
-                password:hashedPassword
-            },
-        });
-        
-        return NextResponse.json({ message: "User created Successfully!", success:true });
-
-        
-    } catch (error:any) {
-        return NextResponse.json({error:error.message},
-            {status:500})
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "user already exists" },
+        { status: 400 }
+      );
     }
-    
-}
 
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        rollnumber,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return NextResponse.json({
+      message: "User created Successfully!",
+      success: true,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
